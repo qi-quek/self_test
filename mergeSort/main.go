4c5a06664a13da40586f4da842d131c7c8c788fd
Quek Qi <quekqi@spgroup.com.sg>
2022-07-05 11:01:44 +0800

    -m "Edited typo"

--- mergeSort/main.go
+++ mergeSort/main.go
@@ -7,7 +7,7 @@ func main() {
 func mergeSort(refSlice []int) []int { //recursive function - calling itself
 	if len(refSlice) < 2 {
 		return refSlice
-	} // if there is only 1 item in the slice, return back the slice value, no need for further merging
+	} // if there is only 1 item in the slice, return the slice value, no need for further merging
 
 	//spitting the input slice into 2 halves, this is the top half, with index ranging from first data to middle data
 	topHalf := mergeSort(refSlice[:len(refSlice)/2])
@@ -25,12 +25,12 @@ func merge(topHalf []int, bottomHalf []int) []int {
 	var tempMergeSlice []int                                               //variable for temp slice
 	var topHalfIndex int                                                   //int value for top half index
 	var bottomHalfIndex int                                                //int value for bottom half index
-	for topHalfIndex < len(topHalf) && bottomHalfIndex < len(bottomHalf) { //keeps looping thruogh if index cycle value is still less than the reference slice
+	for topHalfIndex < len(topHalf) && bottomHalfIndex < len(bottomHalf) { //keeps looping through if index cycle value is still less than the reference slice
 		if topHalf[topHalfIndex] < bottomHalf[bottomHalfIndex] { //if value of bottom half is bigger than top half
 			tempMergeSlice = append(tempMergeSlice, topHalf[topHalfIndex]) //append smaller value first - so that it is in ascending order
 			topHalfIndex++                                                 //++ index count for cycle through
 
-		} else { //if bottom half value is smaller than tophalf value
+		} else { //if bottom half value is smaller than top half value
 			tempMergeSlice = append(tempMergeSlice, bottomHalf[bottomHalfIndex]) //append smaller value first - so that it is in ascending order
 			bottomHalfIndex++                                                    //++ index count for cycle through
 		}
