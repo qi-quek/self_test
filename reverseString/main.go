309b6c3beaaf599f56fbddcc7cbabb0df0f77d40
Quek Qi <quekqi@spgroup.com.sg>
2022-07-05 17:52:00 +0800

    Add codes in reverseString

--- reverseString/main.go
+++ reverseString/main.go
@@ -1,6 +1,8 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func Reverse(s string) (result string) {
 	for _, v := range s {
@@ -13,9 +15,20 @@ func main() {
 	var result string
 	stringEX := "Low Zhan Peng"
 
-	for _, v := range stringEX {
-		result = string(v) + result //this adds each iteration of "new-alphabet" to the start of the string
+	for firstValCheck := range stringEX[:3] { //if only the first value is needed, etc. only index is needed and not the val
+		fmt.Println("this is check for first value, without 2nd value", firstValCheck)
 	}
+
+	fmt.Println("clear space")
+
+	//for _, v := range stringEX {
+	//	result = string(v) + result //this adds each iteration of "new-alphabet" to the start of the string
+	//}
+
 	fmt.Println(result)
+
+	for pos, char := range "日本\x80語" { //\x80 is an illegal UTF-8 encoding
+		fmt.Printf("character %#U starts at byte position %d\n", char, pos)
+	}
 
 }
